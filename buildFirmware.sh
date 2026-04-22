e0ca3a8b37abdf7ab7d9fa4b242e9fd06b46d44f
RyanMcB8 <ryanmcbride8@icloud.com>
2026-04-22 15:31:31 +0100

    Allowing the system to build on windows.

--- buildFirmware.sh
+++ buildFirmware.sh
@@ -4,19 +4,22 @@
 set -e 
 
 # Setting the parameters
-BUILD_DIR="Firmware/build/BB8"
+BUILD_DIR="build/BB8"
 TARGET="BB8"
 ELF_PATH="$BUILD_DIR/$TARGET.elf"
 
 # Building from the CMkaeLists.txt file.
 echo "Building BB8 project..."
-cmake --build "$BUILD_DIR" --target "$TARGET"
+cd ./Firmware
+cmake -B "$BUILD_DIR" -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake
+cmake --build "$BUILD_DIR" --target "$TARGET" 
 
 # Searching to see if the microcontroller is connected.
 echo "Build complete!   "
 echo "Checking for ST-Link..."
 
-if lsusb | grep -qi "STMicroelectronics ST-LINK"; then
+# if lsusb | grep -qi "STMicroelectronics ST-LINK"; then
+if powershell.exe -Command "Get-PnpDevice -FriendlyName '*ST-Link*'"; then
     echo "ST-Link detected"
 
     # ---- Step 3: Flash ----
